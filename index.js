1909fd11415a6dceeb8f51c3c882483f69dd2c86
nishankadel <nishankadel39@gmail.com>
2022-01-10 21:02:05 +0545

    cors policy added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 require("dotenv").config();
 require("./db/db");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
@@ -14,6 +15,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(cors());
 
 app.use("/api", require("./routes/route"));
 
